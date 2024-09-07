7f475226298f4b0ad6f138e8f7f2400acd4174a7
Harshinii-R <harshinirajeshh@gmail.com>
2024-09-07 20:20:58 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/Gemini-Clone/",
+  build: {
+    rollupOptions: {
+      external: ['@google/generative-ai']
+    }
+  }
 })
